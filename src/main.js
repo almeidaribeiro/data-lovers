544befa6b504150fc3bc4dd2cd49d1f8f39989ed
TatianeTabata <tatianetabata@hotmail.com>
2019-02-28 17:30:50 -0300

    exibir dados dos pokemons

--- src/main.js
+++ src/main.js
@@ -1,4 +1,37 @@
-function getPokemon(){
-    console.log(POKEMON["pokemon"])
+// function getPokemon() {
+//   // for(let i in POKEMON["pokemon"]){
+//     while(POKEMON["pokemon"].length < 151){
+//       console.log("foi")
+//       let pokeGet = POKEMON["pokemon"];
+//       let pokeImg = pokeGet.img;
+//       let pokeName = pokeGet.name;
+//       let pokeType = pokeGet.type;
+//       let pokeTotal = pokeImg + pokeName + pokeType;
+//       document.getElementById("exibir").innerHTML = pokeTotal
+//       console.log(pokeTotal)
+//     }      
+// }
+
+// getPokemon()
+
+window.onload = function(){
+  showProducts();
 }
- getPokemon()
+
+let pokemon = POKEMON["pokemon"];
+
+function getProducts() {
+  return pokemon;
+}
+
+function showProducts() {
+  let productDiv = document.getElementById("exibir");
+  productDiv.innerHTML = `${getProducts().map((pokemon) => `
+      <div class="product-item">  
+        <img src="${pokemon["img"]}" class="product-img" />      
+          <div class="text-name">
+            <h3 class="product-name">${pokemon["type"]}</h3>
+          </div>
+      `).join("")}  
+  `
+}
