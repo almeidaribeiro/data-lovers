1f6380ca3e76782d70ea9710c3bb54327469c808
Boo <almeidaribeiro.b@gmail.com>
2019-02-28 15:30:36 -0300

    funçao getPOkemon feita

--- src/main.js
+++ src/main.js
@@ -1,4 +1,12 @@
-function getPokemon(){
-    console.log(POKEMON["pokemon"])
+function getPokemon() {
+    for(let i in POKEMON["pokemon"]){
+        let pokeGet = POKEMON["pokemon"][i];
+        let pokeImg = pokeGet.img;
+        let pokeName = pokeGet.name;
+        let pokeType = pokeGet.type;
+      
+        console.log(pokeImg, pokeName, pokeType)
+    }
 }
+
  getPokemon()
