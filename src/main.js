77272c3de2e2772569a1a37f6a3cb395da58e094
Boo <almeidaribeiro.b@gmail.com>
2019-03-05 23:39:39 -0300

    funçao filtro dos tipos dos pokemons funcionando

--- src/main.js
+++ src/main.js
@@ -12,18 +12,106 @@
 //getPokemon()
 
 window.onload = function(){
-  showProducts();
+  showPokemons();
 }
 
 let pokemon = POKEMON["pokemon"];
 
-function getProducts() {
+function getPokemons(){
   return pokemon;
 }
 
-function showProducts() {
+function showPokemons(){
   let pokeDiv = document.getElementById("display");
-  pokeDiv.innerHTML = `${getProducts().map((pokemon) => `
+  pokeDiv.innerHTML = `${getPokemons().map((pokemon) => `
+      <div class="poke-list">  
+        <img src="${pokemon["img"]}" class="poke-img" />  
+          <div class="text-name">
+          <h3 class="poke-name">${pokemon["name"]}</h3>    
+          <div class="text-type">
+            <h3 class="poke-type">${pokemon["type"]}</h3>
+          </div>
+      `).join("")}  
+  `
+}
+
+//document.getElementById("types").addEventListener("click", getFilteredPokemons);
+
+//function getFilteredPokemons() {
+  
+  //let firePokemons = []
+  //let type = "Fire"
+  //for(let i in POKEMON["pokemon"]){
+    //let poke = POKEMON["pokemon"][i];
+    //let pokeType = poke.type;
+      //if(pokeType.includes(type)){
+        //firePokemons.push(poke)
+      //}
+  //}
+  //console.log(firePokemons)
+ //return firePokemons;
+//}
+
+document.getElementById("btn-fire").addEventListener("click", function(event){
+  showFilteredPokemons("Fire")  
+});
+document.getElementById("btn-grass").addEventListener("click", function(event){
+  showFilteredPokemons("Grass")  
+});
+document.getElementById("btn-poison").addEventListener("click", function(event){
+  showFilteredPokemons("Poison")  
+});
+document.getElementById("btn-ice").addEventListener("click", function(event){
+  showFilteredPokemons("Ice")  
+});
+document.getElementById("btn-flying").addEventListener("click", function(event){
+  showFilteredPokemons("Flying")  
+});
+document.getElementById("btn-psychic").addEventListener("click", function(event){
+  showFilteredPokemons("Psychic")  
+});
+document.getElementById("btn-water").addEventListener("click", function(event){
+  showFilteredPokemons("Water")  
+});
+document.getElementById("btn-rock").addEventListener("click", function(event){
+  showFilteredPokemons("Rock")  
+});
+document.getElementById("btn-ground").addEventListener("click", function(event){
+  showFilteredPokemons("Ground")  
+});
+document.getElementById("btn-electric").addEventListener("click", function(event){
+  showFilteredPokemons("Electric")  
+});
+document.getElementById("btn-bug").addEventListener("click", function(event){
+  showFilteredPokemons("Bug")  
+});
+document.getElementById("btn-normal").addEventListener("click", function(event){
+  showFilteredPokemons("Normal")  
+});
+document.getElementById("btn-fighting").addEventListener("click", function(event){
+  showFilteredPokemons("Fighting")  
+});
+document.getElementById("btn-dragon").addEventListener("click", function(event){
+  showFilteredPokemons("Dragon")  
+});
+document.getElementById("btn-ghost").addEventListener("click", function(event){
+  showFilteredPokemons("Ghost")  
+});
+
+function getFilteredPokemons(pkmType){
+  pokeTypes = pokemon.filter(function(pkm){
+    if(pkm.type.includes(pkmType)){
+      return true
+    }else{
+      return false
+    }
+  })
+  return pokeTypes
+}
+
+function showFilteredPokemons(pkmType) {
+  let pokeDiv = document.getElementById("types");
+  pokeDiv.innerHTML = `${getFilteredPokemons(pkmType).map((pokemon) => `
       <div class="poke-list">  
         <img src="${pokemon["img"]}" class="poke-img" />  
           <div class="text-name">
