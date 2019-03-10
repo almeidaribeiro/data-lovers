cc126fd0fa0db6ad5a9b4fded72c22041b79ac11
Boo <almeidaribeiro.b@gmail.com>
2019-03-10 17:20:28 -0300

    css: img alinhadas

--- src/main.js
+++ src/main.js
@@ -1,183 +1,82 @@
-//function getPokemon() {
-//  for(let i in POKEMON["pokemon"]){
-//    let pokeGet = POKEMON["pokemon"][i];
-//  let pokeImg = pokeGet.img;
-//  let pokeName = pokeGet.name;
-//  let pokeType = pokeGet.type;
+let LAST = null;
 
-// console.log(pokeImg, pokeName, pokeType)
-// }
-//}
+let data = POKEMON["pokemon"];
 
-//getPokemon()
-
-window.onload = function(){
-  showPokemons();
+window.onload = function () {
+  showPokemons(data);
 }
+document.getElementById("btn-all").addEventListener("click", () => getFilteredPokemons("All"));
+document.getElementById("btn-fire").addEventListener("click", () => getFilteredPokemons("Fire"));
+document.getElementById("btn-grass").addEventListener("click", () => getFilteredPokemons("Grass"));
+document.getElementById("btn-poison").addEventListener("click", () => getFilteredPokemons("Poison"));
+document.getElementById("btn-ice").addEventListener("click", () => getFilteredPokemons("Ice"));
+document.getElementById("btn-flying").addEventListener("click", () => getFilteredPokemons("Flying"));
+document.getElementById("btn-psychic").addEventListener("click", () => getFilteredPokemons("Psychic"));
+document.getElementById("btn-water").addEventListener("click", () => getFilteredPokemons("Water"));
+document.getElementById("btn-rock").addEventListener("click", () => getFilteredPokemons("Rock"));
+document.getElementById("btn-ground").addEventListener("click", () => getFilteredPokemons("Ground"));
+document.getElementById("btn-electric").addEventListener("click", () => getFilteredPokemons("Electric"));
+document.getElementById("btn-bug").addEventListener("click", () => getFilteredPokemons("Bug"));
+document.getElementById("btn-normal").addEventListener("click", () => getFilteredPokemons("Normal"));
+document.getElementById("btn-fighting").addEventListener("click", () => getFilteredPokemons("Fighting"));
+document.getElementById("btn-dragon").addEventListener("click", () => getFilteredPokemons("Dragon"));
+document.getElementById("btn-ghost").addEventListener("click", () => getFilteredPokemons("Ghost"));
+document.querySelector("#sort").addEventListener("change", () => getFilteredPokemons(null));
 
-let pokemon = POKEMON["pokemon"];
+function getFilteredPokemons(pkmType) {
+  const filterBy = pkmType || LAST || "All";
+  LAST = filterBy;
 
-function getPokemons(){
-  return pokemon;
-}
-
-function showPokemons(){
-  let pokeDiv = document.getElementById("display");
-  pokeDiv.innerHTML = `${getPokemons().map((pokemon) => `
-      <div class="poke-list">  
-        <img src="${pokemon["img"]}" class="poke-img" />  
-          <div class="text-name">
-          <h3 class="poke-name">${pokemon["name"]}</h3>    
-          <div class="text-type">
-            <h3 class="poke-type">${pokemon["type"]}</h3>
-          </div>
-      `).join("")}  
-  `
-}
-
-//document.getElementById("types").addEventListener("click", getFilteredPokemons);
-
-//function getFilteredPokemons() {
+  let pokeTypes = data;
   
-  //let firePokemons = []
-  //let type = "Fire"
-  //for(let i in POKEMON["pokemon"]){
-    //let poke = POKEMON["pokemon"][i];
-    //let pokeType = poke.type;
-      //if(pokeType.includes(type)){
-        //firePokemons.push(poke)
-      //}
-  //}
-  //console.log(firePokemons)
- //return firePokemons;
-//}
-
-document.getElementById("btn-fire").addEventListener("click", function(event){
-  showFilteredPokemons("Fire")  
-});
-document.getElementById("btn-grass").addEventListener("click", function(event){
-  showFilteredPokemons("Grass")  
-});
-document.getElementById("btn-poison").addEventListener("click", function(event){
-  showFilteredPokemons("Poison")  
-});
-document.getElementById("btn-ice").addEventListener("click", function(event){
-  showFilteredPokemons("Ice")  
-});
-document.getElementById("btn-flying").addEventListener("click", function(event){
-  showFilteredPokemons("Flying")  
-});
-document.getElementById("btn-psychic").addEventListener("click", function(event){
-  showFilteredPokemons("Psychic")  
-});
-document.getElementById("btn-water").addEventListener("click", function(event){
-  showFilteredPokemons("Water")  
-});
-document.getElementById("btn-rock").addEventListener("click", function(event){
-  showFilteredPokemons("Rock")  
-});
-document.getElementById("btn-ground").addEventListener("click", function(event){
-  showFilteredPokemons("Ground")  
-});
-document.getElementById("btn-electric").addEventListener("click", function(event){
-  showFilteredPokemons("Electric")  
-});
-document.getElementById("btn-bug").addEventListener("click", function(event){
-  showFilteredPokemons("Bug")  
-});
-document.getElementById("btn-normal").addEventListener("click", function(event){
-  showFilteredPokemons("Normal")  
-});
-document.getElementById("btn-fighting").addEventListener("click", function(event){
-  showFilteredPokemons("Fighting")  
-});
-document.getElementById("btn-dragon").addEventListener("click", function(event){
-  showFilteredPokemons("Dragon")  
-});
-document.getElementById("btn-ghost").addEventListener("click", function(event){
-  showFilteredPokemons("Ghost")  
-});
-
-function getFilteredPokemons(pkmType){
-  pokeTypes = pokemon.filter(function(pkm){
-    if(pkm.type.includes(pkmType)){
-      return true
-    }else{
-      return false
-    }
-  })
-  return pokeTypes
-}
-
-function showFilteredPokemons(pkmType) {
-  let pokeDiv = document.getElementById("types");
-  pokeDiv.innerHTML = `${getFilteredPokemons(pkmType).map((pokemon) => `
-      <div class="poke-list">  
-        <img src="${pokemon["img"]}" class="poke-img" />  
-          <div class="text-name">
-          <h3 class="poke-name">${pokemon["name"]}</h3>    
-          <div class="text-type">
-            <h3 class="poke-type">${pokemon["type"]}</h3>
-          </div>
-      `).join("")}  
-  `
-}
-
-//let poke = pokemon.sort(function(a,b){return a.name - b.name});
-//console.log(poke);
-
-//function orderPkm(){
-//let pokemonOrder = pokemon.sort((a.b)=> {
-  //if(a.name > b.name){
-    //return 1;
-  //}
-    //return -1;
-//)
-//}
-
-//criar um evento
-document.getElementById("sort").addEventListener("change", function(event){
-  showOderPokemons(event)
-});
-
-function sortPokemons(event){
-  const selectedValue = event.target.value;
+  if (filterBy != "All"){
+     pokeTypes = data.filter((pkm) => pkm.type.includes(filterBy));
+  }
   
-  if(selectedValue === "A-Z"){
-    pokemons = POKEMON["pokemon"].sort(function(a,b){
+  sortPokemons(pokeTypes);
+}
+
+function sortPokemons(content) {
+  const select = document.getElementById("sort");
+  const selectedValue = select.options[select.selectedIndex].value;
+
+  if (selectedValue === "A-Z") {
+    pokemons = content.sort(function (a, b) {
       return a.name.localeCompare(b.name);
     })
-  }else {
-    pokemons = POKEMON["pokemon"].sort(function(a,b){
+  } else {
+    pokemons = content.sort(function (a, b) {
       return b.name.localeCompare(a.name);
     })
   }
-  return pokemons
+  showPokemons(pokemons);
 }
 
-function showOderPokemons(event) {
-  let pokeDiv = document.getElementById("order");
-  pokeDiv.innerHTML = `${sortPokemons(event).map((pokemon) => `
-      <div class="poke-list">  
+function showPokemons(data) {
+  let pokemonList = document.getElementById("pokemon-list");
+  pokemonList.innerHTML = `${data.map((pokemon) => `
+      <li class="pokemon">  
         <img src="${pokemon["img"]}" class="poke-img" />  
-          <div class="text-name">
-          <h3 class="poke-name">${pokemon["name"]}</h3>    
-          <div class="text-type">
-            <h3 class="poke-type">${pokemon["type"]}</h3>
-          </div>
-      `).join("")}  
-  `
+        <h3 class="poke-name">${pokemon["name"]}</h3>      
+        <h3 class="poke-type">${pokemon["type"].join(" | ")}</h3>
+      </li>
+      `).join("")}`
 }
 
-let media = document.getElementById("media")
-media.addEventListener("click", showAverage)
+// //let media = document.getElementById("media")
+// //media.addEventListener("click", showAverage)
 
-function showAverage(){
-  let sum = pokemon.reduce(
-  (total, altura) => total = (total + parseFloat(altura.height)),
-  0);
-  let average = sum/151
-  let totalAverage = document.getElementById("averageHeight")
-  totalAverage.innerHTML = "Média de " + parseFloat(average.toFixed(2)) + " metros"
-}
+// function showAverage() {
+//   const select = document.getElementById("media");
+//   const selectedValue = select.options[select.selectedIndex].value;
 
+//   if(selectedValue === "altura"){
+//   let sum = pokemon.reduce(
+//     (total, altura) => total = (total + parseFloat(altura.height)),
+//     0);
+//   } 
+//   let average = sum / 151
+//   let totalAverage = document.getElementById("averageHeight")
+//   totalAverage.innerHTML = "Média de " + parseFloat(average.toFixed(2)) + " metros"
+  
+// }
